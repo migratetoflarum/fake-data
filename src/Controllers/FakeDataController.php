8c8ce1adb648e95ea5ba8a55f8173ef301d7de01
Clark Winkelmann <clark.winkelmann@gmail.com>
2020-03-08 23:30:24 +0100

    Update Zend namespace and require beta 12

--- src/Controllers/FakeDataController.php
+++ src/Controllers/FakeDataController.php
@@ -16,7 +16,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Symfony\Component\Translation\TranslatorInterface;
-use Zend\Diactoros\Response\EmptyResponse;
+use Laminas\Diactoros\Response\EmptyResponse;
 
 class FakeDataController implements RequestHandlerInterface
 {
